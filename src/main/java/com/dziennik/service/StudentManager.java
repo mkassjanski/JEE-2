006e9456181aed3e3c860e00f4df0c1781f92f39
mkassjanski <ankoupl@o2.pl>
2016-01-09 16:27:09 +0100

    Dodanie EntityManager'a

---
+++ src/main/java/com/dziennik/service/StudentManager.java
@@ -0,0 +1,34 @@
+package com.dziennik.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.dziennik.domain.Student;
+
+@Stateless
+public class StudentManager {
+    @PersistenceContext
+    EntityManager em;
+    
+	public void addStudent(Student student) {
+		student.setId(null);
+		em.persist(student);
+	}
+	public void updateStudent(Student student){
+		student = em.find(Student.class, student.getId());
+		em.merge(student);
+	}
+	
+	public void deleteStudent(Student student) {
+		student = em.find(Student.class, student.getId());
+		em.remove(student);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Student> getAllPersons() {
+		return em.createNamedQuery("student.all").getResultList();
+	}
+}
